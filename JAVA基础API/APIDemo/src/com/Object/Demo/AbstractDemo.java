e42427cf0d20c0391db591180cf56d03b032fe91
hangzhouceshi02 <hangzhouceshi02@163.com>
2019-10-30 23:37:23 +0800

    201910302337

---
+++ JAVA基础API/APIDemo/src/com/Object/Demo/AbstractDemo.java
@@ -0,0 +1,12 @@
+package com.Object.Demo;
+
+public abstract class AbstractDemo {
+
+    // 这是一个抽象方法
+    public abstract void func();
+
+    // 这是一个普通的成员方法
+    public void normalMethod(){
+        System.out.println("heelo");
+    }
+}
